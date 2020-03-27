b625d7acce78d52b6ed11b864b048301d6d6abfd
Justin Warren <justinwarren@Justins-MacBook-Pro.local>
2020-03-27 17:39:00 -0400

    Updates

---
+++ precache-manifest.2dc598c9e4c11f2c3b4ae75bb1883ad1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff15625110250584ee0ac66d377e44f",
+    "url": "/my-jamz-app/index.html"
+  },
+  {
+    "revision": "6d91bfe16814e1eba61a",
+    "url": "/my-jamz-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88a6a33cad91673edfd6",
+    "url": "/my-jamz-app/static/js/2.4f5846ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-jamz-app/static/js/2.4f5846ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d91bfe16814e1eba61a",
+    "url": "/my-jamz-app/static/js/main.7fad8ece.chunk.js"
+  },
+  {
+    "revision": "88738e9b4e2d3fffcc0a",
+    "url": "/my-jamz-app/static/js/runtime-main.8bdc43e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-jamz-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
