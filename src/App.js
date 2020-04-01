cc2ff0fb02e6c6d20078df61f997da080fb19fbd
Justin Warren <justinwarren@Justins-MacBook-Pro.local>
2020-04-01 17:40:32 -0400

    added bootstrap and font awesome to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FaChess } from 'react-icons/fa';
+
+
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <header className="App-header">
         <h1>{"Hello!"}</h1>
         <h2>{"I am a React."}</h2>
+        <button className="btn btn-danger">Submit</button>
+        <h3> Lets play <FaChess />? </h3>
       </header>
     </div>
   );
